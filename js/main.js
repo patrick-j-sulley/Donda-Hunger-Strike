5f1f68a5321af59f26ee5ad7b65893182e7a9a0e
patrick-j-sulley <dxhhhshawn@gmail.com>
2021-07-29 16:43:46 +1200

    Added countdown from original date function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 var totalDaysSince = 0;
+var totalHoursSince = 0;
+var totalMinsSince = 0;
+var totalSecsSince = 0;
 
-function calculateDaysSince(){
+var originalDate = new Date('July 23, 2021');
+var todaysDate = new Date(Date.now())
 
-    let originalDate = new Date('July 23, 2021');
-    let todaysDate = new Date(Date.now())
+var days, hours, mins, secs;
 
+
+function calculateDaysSince() {
 
     let oneDay = 1000 * 60 * 60 * 24;
 
@@ -12,40 +17,70 @@ function calculateDaysSince(){
 
     totalDaysSince = Math.round(timeDifference / oneDay) - 1;
 
+    totalHoursSince = Math.round(todaysDate.getHours())
+
+    totalMinsSince = Math.round(todaysDate.getMinutes())
+
+    totalSecsSince = Math.round(todaysDate.getSeconds())
+
     document.getElementById("total").innerHTML = totalDaysSince;
     document.title = 'DONDA Hunger Strike Day ' + totalDaysSince
 
 }
 
-function calculateProtesterHunger(days){
+function calculateProtesterHunger(days) {
 
-    if(days <= 3)
-    {
+    if (days <= 3) {
         document.getElementById("protester").src = "./images/hungerstrike1-3.jpg"
     }
-    else if(days <= 7)
-    {
+    else if (days <= 7) {
         document.getElementById("protester").src = "./images/hungerstrike4-7.jpg"
     }
-    else if(days <= 12)
-    {
+    else if (days <= 12) {
         document.getElementById("protester").src = "./images/hungerstrike8-12.jpg"
     }
-    else if(days <= 17)
-    {
+    else if (days <= 17) {
         document.getElementById("protester").src = "./images/hungerstrike13-17.jpg"
     }
-    else if(days <= 21)
-    {
+    else if (days <= 21) {
         document.getElementById("protester").src = "./images/hungerstrike18-21.jpg"
     }
-    else
-    {
+    else {
         document.getElementById("protester").src = "./images/hungerstrike22.jpg"
     }
 
 }
 
+function startCount() {
+    days = totalDaysSince;
+    hours = totalHoursSince;
+    mins = totalMinsSince;
+    secs = totalSecsSince;
+    timer = setInterval(count, 1000);
+}
+function count() {
+
+
+
+    secs++;
+    if (secs == 60) {
+        secs = 0;
+        mins = mins + 1;
+    }
+    if (mins == 60) {
+        mins = 0;
+        hours = hours + 1;
+    }
+    if (hours == 13) {
+        hours = 1;
+    }
+
+    document.getElementById('timer').innerHTML = (days + " Days, " + hours + " Hours, " + mins + " Minutes, and " + secs + " Seconds.");
+
+}
+
+
 
 calculateDaysSince()
 calculateProtesterHunger(totalDaysSince)
+startCount();
